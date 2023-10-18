5f4e2552c6fdfba56f389352bce4eff52660ac7e
эспада8888 <sardorbekkhotamov77@gmail.com>
2023-10-18 21:35:17 +0500

    removed

--- index.js
+++ index.js
@@ -21,21 +21,21 @@
 //  alert('сколько снял?')
 // }
 // }
-let a =prompt("введите вашу первую букву");
+// let a =prompt("введите вашу первую букву");
 
-if (a.charAt(0).toLowerCase()==="a"){
-    let old =prompt('Сколько вам лет ?');
-    old=parseInt(old);
-    if (old >=20 && old <=40){
-        let money =prompt("Сколько у вас денег ?")
-        money=parseInt(money);
-        if(money >=100){
-            let how_much=prompt('Сколько вас ?')
-            if(how_much <=10){
-                alert('добро пожаловать')
-            }else{
-                alert('dosvidos')
-            }
-        }
-    }
-}
+// if (a.charAt(0).toLowerCase()==="a"){
+//     let old =prompt('Сколько вам лет ?');
+//     old=parseInt(old);
+//     if (old >=20 && old <=40){
+//         let money =prompt("Сколько у вас денег ?")
+//         money=parseInt(money);
+//         if(money >=100){
+//             let how_much=prompt('Сколько вас ?')
+//             if(how_much <=10){
+//                 alert('добро пожаловать')
+//             }else{
+//                 alert('dosvidos')
+//             }
+//         }
+//     }
+// }
