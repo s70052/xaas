ddec41016d822ab10448ac8438a8578abfca32a6
эспада8888 <sardorbekkhotamov77@gmail.com>
2023-10-18 20:34:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// let money =10000
+// let name =prompt('Введите ваше имя');
+// if(name.toLowerCase().trim() !=='alex'){
+//     alert('Пользователь не найден');
+// } else{
+//     let num =prompt('номер счета ?');
+//   num=parseInt(num);
+
+//     if(num !== 7777){
+//         alert('Пользователь не найден');
+//     }else{
+//     let number =prompt('Сколько обналичить ?');
+//     number=parseInt(number);
+//  if(number >=0 && number <=10000){
+//     let balance= money-number;
+//     alert('все отлично');
+//  }else{
+//     alert('Недостаточно средств')
+//  } 
+//  alert('cколько осталось?');
+//  alert('сколько снял?')
+// }
+// }
+// let a =prompt("введите вашу первую букву");
+
+// if (a.charAt(0).toLowerCase()==="a"){
+
+//     let old =prompt('Сколько вам лет ?');
+//     old=parseInt(old);
+//     if (old >20 && old <40){
+//         let money =prompt("Сколько у вас денеги ?")
+//         money=parseInt(money);
+//         if(money >100){
+//             let how_much=prompt('Сколько вас ?')
+//             if(how_much <=10){
+//                 alert('добро пожаловать')
+//             }
+//         }
+//     }
+// }
