db849044816ab3bea0ee0bb38ea064607622d37c
эспада8888 <sardorbekkhotamov77@gmail.com>
2023-10-19 15:43:50 +0500

    removed github

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-// let money =10000
-// let name =prompt('Введите ваше имя');
-// if(name.toLowerCase().trim() !=='alex'){
-//     alert('Пользователь не найден');
-// } else{
-//     let num =prompt('номер счета ?');
-//   num=parseInt(num);
+let money =10000
+let name =prompt('Введите ваше имя');
+if(name.toLowerCase().trim() !=='alex'){
+    alert('Пользователь не найден');
+} else{
+    let num =prompt('номер счета ?');
+  num=parseInt(num);
 
-//     if(num !== 7777){
-//         alert('Пользователь не найден');
-//     }else{
-//     let number =prompt('Сколько обналичить ?');
-//     number=parseInt(number);
-//  if(number >=0 && number <=10000){
-//     let balance= money-number;
-//     alert('все отлично');
-//  }else{
-//     alert('Недостаточно средств')
-//  } 
-//  alert('cколько осталось?');
-//  alert('сколько снял?')
-// }
-// }
+    if(num !== 7777){
+        alert('Пользователь не найден');
+    }else{
+    let number =prompt('Сколько обналичить ?');
+    number=parseInt(number);
+ if(number >=0 && number <=10000){
+    alert('все отлично');
+     alert('cколько осталось?');
+ alert('сколько снял?')
+ }else{
+    alert('Недостаточно средств')
+ } 
+}
+}
 // let a =prompt("введите вашу первую букву");
 
 // if (a.charAt(0).toLowerCase()==="a"){
